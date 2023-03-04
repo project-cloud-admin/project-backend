a324aaeddce868ea8d955f748cbaf2358b295d93
EmiliaMM <91829290+EmiliaMM@users.noreply.github.com>
2023-03-04 15:40:21 +0100

    Add files via upload

--- hello.py
+++ hello.py
@@ -1,7 +1,26 @@
-from flask import Flask
+from flask import Flask, request, render_template, escape
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@app.route('/hello', methods=['POST', 'GET'])
+def hello():
+    if request.method == 'GET':
+        return render_template('form.html')
+    if request.method == 'POST':
+        name = request.form['name']
+        return f'Hello {escape(name)}'
+    return render_template('form.html')
+
+if __name__ == "__main__":
+    app.run()
+
+    
+    
+        
+
+
