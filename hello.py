40288de03650d501b822f8a8eccca4ead0ddccc3
EmiliaMM <91829290+EmiliaMM@users.noreply.github.com>
2023-03-04 15:48:57 +0100

    Update hello.py

--- hello.py
+++ hello.py
@@ -1,14 +1,15 @@
 from flask import Flask, request, render_template, escape
 
 app = Flask(__name__)
-
+'''
+endpoint
+'''
 @app.route('/')
 def hello_world():
     return "<p>Hello, World!</p>"
-
-
 @app.route('/hello', methods=['POST', 'GET'])
 def hello():
+    '''get'''
     if request.method == 'GET':
         return render_template('form.html')
     if request.method == 'POST':
@@ -18,9 +19,4 @@ def hello():
 
 if __name__ == "__main__":
     app.run()
-
     
-    
-        
-
-
